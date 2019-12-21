3763eaed3397dda4c19ccbacf063a96e4cfd1d08
Kamil Celejewski <kamil12283@gmail.com>
2019-12-22 00:02:36 +0100

    [Add] draw red square on click

--- hello_tkinter.py
+++ hello_tkinter.py
@@ -3,7 +3,11 @@ import tkinter as tk
 
 root = tk.Tk()
 
+def xy(event):
+    canvas.create_rectangle(event.x - 3, event.y - 3, event.x + 3, event.y + 3, fill="red")
+
 canvas = tk.Canvas(root, height=400, width=400, bg="green")
 canvas.pack()
+canvas.bind("<Button-1>", xy)
 
 root.mainloop()
