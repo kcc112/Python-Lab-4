e42f4df7a775e65b96d66a82d2fc93c5852f5faa
Kamil Celejewski <kamil12283@gmail.com>
2019-12-22 15:56:27 +0100

    [Add] step button

--- hello_tkinter.py
+++ hello_tkinter.py
@@ -11,19 +11,19 @@ class Field:
         self.size = 2 * (1.5 * init_pos_limit)
         self.half_size = self.size // 2
         self.sheeps = []
-        self.canvas = tk.Canvas(root, height=self.size, width=self.size, bg="green")
+        self.canvas = tk.Canvas(root, height=self.size, width=self.size, bg='green')
         self.canvas.bind('<Button-1>', self.add_sheep)
         self.canvas.bind('<Button-3>', self.change_wolf_position)
-        self.canvas.pack()
+        self.canvas.grid(row=0, column=0, sticky='W')
         self.wolf = self.canvas.create_rectangle(self.half_size - 2, self.half_size - 2, self.half_size + 2, self.half_size + 2, fill='red')
 
     def add_sheep(self, event):
         self.sheeps.append(self.canvas.create_rectangle(
-            event.x - 2, event.y - 2, event.x + 2, event.y + 2, fill="blue"))
+            event.x - 2, event.y - 2, event.x + 2, event.y + 2, fill='blue'))
 
     def change_wolf_position(self, event):
         self.canvas.coords(self.wolf, event.x - 2, event.y - 2, event.x + 2, event.y + 2)
 
-
+button = tk.Button(root, text = 'Step').grid(row=0, column=1)
 field = Field()
 root.mainloop()
